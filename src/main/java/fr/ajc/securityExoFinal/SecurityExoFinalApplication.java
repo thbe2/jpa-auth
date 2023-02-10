db4d1102e5782c3996e7d0a4966f3d6880b1a38b
thbe2 <theo.besson@gmx.fr>
2023-02-10 15:00:44 +0100

    Commit initial

---
+++ src/main/java/fr/ajc/securityExoFinal/SecurityExoFinalApplication.java
@@ -0,0 +1,13 @@
+package fr.ajc.securityExoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ajc/securityExoFinal/SecurityExoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ajc.securityExoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
